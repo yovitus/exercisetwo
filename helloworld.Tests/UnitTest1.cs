1735ad0878d94b244ff0c871b9190ed364224a6a
yovitus <vime@itu.dk>
2022-09-06 14:56:57 +0200

    Implemented error handling

--- helloworld.Tests/UnitTest1.cs
+++ helloworld.Tests/UnitTest1.cs
@@ -1,10 +1,43 @@
 namespace helloworld.test;
-
+using helloworld;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void test1()
     {
+        //Arange
+        
+    
+        //Act
+        var result = Program.isYearLeapYear(1600);
+        var resultTwo = Program.isYearLeapYear(1601);
 
+        //Assert
+        result.Should().Be(true);
+        resultTwo.Should().Be(false);
+    }
+
+    [Fact]
+    public void test2()
+    {
+        int isYearLeapYear1 = 1500;
+        int isYearLeapYear2 = 1600;
+        
+        Action action1 = () => Program.lessThan(isYearLeapYear1);
+        Action action2 = () => Program.lessThan(isYearLeapYear2);
+
+        action1.Should().Throw<Program.lessThanException>("Year too low");
+        action2.Should().NotThrow<Program.lessThanException>("Year is above 1587");
+
+    }
+
+    [Fact]
+    public void test3()
+    {
+        String input = "Hello";
+
+        Action action1 = () => Program.isYearLeapYear(Int32.Parse(input));
+
+        action1.Should().Throw<System.FormatException>("Input is not and int");
     }
 }
--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+namespace helloworld;
+
+public class Program {
+
+    public static void Main(string[] args) {
+        
+        try {
+        Console.WriteLine("Enter year: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+        lessThan(input);
+        bool answer = isYearLeapYear(input);
+        if(!answer) Console.WriteLine("Nay");
+        if(answer) Console.WriteLine("Yay");
+        } catch (System.FormatException) {
+            Console.WriteLine("Not an integer");
+        }
+    }
+
+    public static void lessThan(int year) {
+        if(year <= 1587) {
+            throw new lessThanException("Leap year must be after the year 1587");
+        }
+    }
+
+    public static bool isYearLeapYear(int year) {
+        var output = false;
+    
+        if(year % 4 == 0) {
+            if(year % 100 == 0) {
+                if(year % 400 == 0) {
+                    output = true;
+                }
+                else {
+                    output = false;
+                }
+            }
+            else {
+                output = true;
+            }
+        } else {
+            output = false;
+        }
+        return output;
+    
+    }
+
+    public class lessThanException: Exception {
+
+        public lessThanException(string message): base (message)
+        {
+            //message = "Leap year must be after the year 1587";
+        }
+
+    }
+}
